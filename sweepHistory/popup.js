83a80ccc85a355466210a3d9412fe1ae68f91f6f
Rajesh <mvrrajeshmvr23@gmail.com>
2023-04-12 07:53:23 +0530

    cookies added

--- sweepHistory/popup.js
+++ sweepHistory/popup.js
@@ -6,8 +6,19 @@ document.getElementById('remove').onclick = () => {
 }
 document.getElementById("cookies").onclick=()=>{
     chrome.cookies.getAll({},cookies)
-      
 }
+
 function cookies(cookies){
      console.log(cookies)
 }
+let removes=document.getElementById("cookiesRemove");
+removes.addEventListener("click",()=>{
+  chrome.cookies.remove({
+    url: "https://www.google.com/",
+    name: "myCookie",
+    path: "/subdirectory"
+  }, function(removedCookie) {
+    console.log("Removed cookie:", removedCookie);
+  });
+  
+})
